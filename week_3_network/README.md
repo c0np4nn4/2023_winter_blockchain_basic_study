d90bc68a1a64b2e720995c910edcaa4c98ecfa56
c0np4nn4 <c0np4nn4@gmail.com>
2024-02-01 15:49:03 +0900

    add: Week3, README

---
+++ week_3_network/README.md
@@ -0,0 +1,12 @@
+# Network
+
+- Flask 를 이용해 `API 서버`를 만듦
+
+---
+
+## Request
+```bash
+curl http://127.0.0.1/get_block/1
+
+<genesis_block>
+```
--- week_3_network/peer.py
+++ week_3_network/peer.py
@@ -1,18 +1,38 @@
 from flask import Flask
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'week_2_block_structure')))
+# from block import block
+from get_block import ledger.py
 
 app = Flask(__name__)
 
-@app.route('/get_block')
-def hello() -> str:
-    return "Hello! This is Flask!";
+@app.route('/get_block/<block_number>')
+def get_block(block_number) -> str:
+    return f"Hello! This is Flask for /get_block route. blocknumeber: {block_number}"
 
-# @app.route('/ledger_append')
-# def hello() -> str:
-#     return "Hello! This is Flask!";
+@app.route('/ledger_append')
+def ledger_append() -> str:
+    return "Hello! This is Flask for /ledger_append route."
 
-# @app.route('/get_last_block')
-# def hello() -> str:
-#     return "Hello! This is Flask!";
+@app.route('/get_last_block')
+def get_last_block() -> str:
+    return "Hello! This is Flask for /get_last_block route."
 
-app.run()
+# TODO
+# ledger.py 에 있는 함수들 호출할 수 있도록 하기
+### [!] get_block
+### [ ] get_latest_block
+### [ ] ledger_append
+
+# TODO
+# block.py 에 있는 함수 중
+### [ ] gen_block_header
+### [ ] gen_block_data
+### [ ] gen_block
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
